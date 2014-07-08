4f15bc01ec15325dae8fa1c733335d54cf6e2ed1
Matthew Davies <daviesgeek@icloud.com>
2014-07-08 14:33:01 -0700

    Initial Commit

---
+++ DotFiles/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DotFiles
+//
+//  Created by Matthew Davies on 7/8/14.
+//  Copyright (c) 2014 Matthew Davies. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DotFiles/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  DotFiles
+//
+//  Created by Matthew Davies on 7/8/14.
+//  Copyright (c) 2014 Matthew Davies. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
