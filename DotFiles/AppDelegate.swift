f51450438238c73315c6f8f8f9efedfce8049091
Matthew Davies <daviesgeek@icloud.com>
2014-07-09 16:18:24 -0700

    Added implementation of ignoredFiles Code cleanup

--- DotFiles/AppDelegate.swift
+++ DotFiles/AppDelegate.swift
@@ -11,63 +11,67 @@ import AppKit
 
 class AppDelegate: NSObject, NSApplicationDelegate {
   
-  // All the UI Elements \\
+// All the UI Elements \\
   
-    @IBOutlet var selectSource: NSButton
-    @IBOutlet var selectDest: NSButton
+  @IBOutlet var selectSource: NSButton
+  @IBOutlet var selectDest: NSButton
+  @IBOutlet var window: NSWindow
 
-    @IBOutlet var sourcePath: NSTextField
-    @IBOutlet var destPath: NSTextField
-  
-    @IBOutlet var linkButton: NSButton
+  @IBOutlet var sourcePath: NSTextField
+  @IBOutlet var destPath: NSTextField
 
-    // Initialize all the global stuff
-    var sourceFolder: NSURL = NSURL()
-    var destFolder = NSHomeDirectory()
-    var manager: NSFileManager = NSFileManager()
-    var ignoredFiles = [
-      ".git": true,
-      ".gitignore": true,
-      ".DS_Store": true
-    ]
-  
-    func applicationDidFinishLaunching(aNotification: NSNotification?) {
-      
-        // Set up everything for displaying/hiding
-        sourcePath.hidden = true
-        destPath.stringValue = destFolder
-        linkButton.enabled = false
-    }
+  @IBOutlet var linkButton: NSButton
 
-    func applicationWillTerminate(aNotification: NSNotification?) {
-        // Insert code here to tear down your application
-    }
+  // Initialize all the global stuff
+  var sourceFolder: NSURL = NSURL()
+  var destFolder = NSHomeDirectory()
+  var manager: NSFileManager = NSFileManager()
+  var ignoredFiles = [
+    ".git": true,
+    ".gitignore": true,
+    ".DS_Store": true,
+    "folder": true
+  ]
+
+  func applicationDidFinishLaunching(aNotification: NSNotification?) {
+    
+    // Set up everything for displaying/hiding
+    sourcePath.hidden = true
+    destPath.stringValue = destFolder
+    linkButton.enabled = false
+    window.center()
   
-    // Opens a modal for selecting a source folder
-    @IBAction func selectFolder(sender: AnyObject) {
-      var selector: NSOpenPanel = NSOpenPanel()
-      
-      selector.canChooseDirectories = true
-      if(selector.runModal() == NSOKButton) {
-        sourceFolder = selector.URLs[0] as NSURL
-        sourcePath.stringValue = sourceFolder.path
-        if(manager.fileExistsAtPath(sourceFolder.path) == true) {
-          sourcePath.hidden = false
-          linkButton.enabled = true
-        }
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification?) {
+      // Insert code here to tear down your application
+  }
+  
+  // Opens a modal for selecting a source folder
+  @IBAction func selectFolder(sender: AnyObject) {
+    var selector: NSOpenPanel = NSOpenPanel()
+    
+    selector.canChooseDirectories = true
+    if(selector.runModal() == NSOKButton) {
+      sourceFolder = selector.URLs[0] as NSURL
+      sourcePath.stringValue = sourceFolder.path
+      if(manager.fileExistsAtPath(sourceFolder.path) == true) {
+        sourcePath.hidden = false
+        linkButton.enabled = true
       }
     }
+  }
   
-    // Opens a modal for selecting a destination folder
-    @IBAction func selectDest(sender: AnyObject) {
-      var selector: NSOpenPanel = NSOpenPanel()
-      
-      selector.canChooseDirectories = true
-      if(selector.runModal() == NSOKButton) {
-        destFolder = selector.URLs[0].path
-        destPath.stringValue = destFolder
-      }
+  // Opens a modal for selecting a destination folder
+  @IBAction func selectDest(sender: AnyObject) {
+    var selector: NSOpenPanel = NSOpenPanel()
+    
+    selector.canChooseDirectories = true
+    if(selector.runModal() == NSOKButton) {
+      destFolder = selector.URLs[0].path
+      destPath.stringValue = destFolder
     }
+  }
   
   // Symlink the files
   @IBAction func symlinkFolder(sender: AnyObject) {
@@ -76,6 +80,9 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     files = manager.contentsOfDirectoryAtURL(sourceFolder, includingPropertiesForKeys: nil, options:nil, error:nil)
     for file in files {
       var filePath: String = file.path
+      if ignoredFiles[filePath.lastPathComponent] == true {
+        continue;
+      }
       var destination = destFolder + "/" + filePath.lastPathComponent
       println(filePath)
       println(destination)
