51d44d81ce5f4cd7bbbe2a856fa7bd1d881ccd7c
Matthew Davies <daviesgeek@icloud.com>
2014-07-09 15:15:55 -0700

    Initial interface Does everything except symlinking :)

--- DotFiles/AppDelegate.swift
+++ DotFiles/AppDelegate.swift
@@ -7,20 +7,64 @@
 //
 
 import Cocoa
+import AppKit
 
 class AppDelegate: NSObject, NSApplicationDelegate {
-                            
-    @IBOutlet var window: NSWindow
+  
+  // All the UI Elements \\
+  
+    @IBOutlet var selectSource: NSButton
+    @IBOutlet var selectDest: NSButton
 
+    @IBOutlet var sourcePath: NSTextField
+    @IBOutlet var destPath: NSTextField
+  
+    @IBOutlet var linkButton: NSButton
 
+    // Initialize all the global stuff
+    var sourceFolder: NSURL = NSURL()
+    var homeFolder = NSHomeDirectory()
+    var manager: NSFileManager = NSFileManager()
+  
     func applicationDidFinishLaunching(aNotification: NSNotification?) {
-        // Insert code here to initialize your application
+      
+        // Set up everything for displaying/hiding
+        sourcePath.hidden = true
+        destPath.stringValue = homeFolder
+        linkButton.enabled = false
     }
 
     func applicationWillTerminate(aNotification: NSNotification?) {
         // Insert code here to tear down your application
     }
-
-
+    
+    @IBAction func selectFolder(sender: AnyObject) {
+      var selector: NSOpenPanel = NSOpenPanel()
+      
+      selector.canChooseDirectories = true
+      if(selector.runModal() == NSOKButton) {
+        sourceFolder = selector.URLs[0] as NSURL
+        sourcePath.stringValue = sourceFolder.path
+        if(manager.fileExistsAtPath(sourceFolder.path) == true) {
+          sourcePath.hidden = false
+          linkButton.enabled = true
+        }
+      }
+    }
+  
+    @IBAction func selectDest(sender: AnyObject) {
+      var selector: NSOpenPanel = NSOpenPanel()
+      
+    }
+  
+  @IBAction func symlinkFolder(sender: AnyObject) {
+    var files = []
+    files = manager.contentsOfDirectoryAtURL(sourceFolder, includingPropertiesForKeys: nil, options:nil, error:nil)
+    for file in files {
+      var filePath: String = file.path
+      println(filePath)
+      println(manager.createSymbolicLinkAtPath(filePath, withDestinationPath: homeFolder as String, error: nil))
+    }
+  }
 }
 
---
+++ DotFiles/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DotFiles
+//
+//  Created by Matthew Davies on 7/8/14.
+//  Copyright (c) 2014 Matthew Davies. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+class ViewController: NSWindowController {
+
+    @IBOutlet var window: NSWindow
+    @IBOutlet var selectFolder: NSButtonCell
+    @IBOutlet var sourcePath: NSTextField
+    @IBOutlet var destPath: NSTextField
+
+    
+}
