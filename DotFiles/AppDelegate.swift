7855bbd041ef03f70011d6ce4fea05faef68cd6c
Matthew Davies <daviesgeek@icloud.com>
2014-07-09 15:50:25 -0700

    Now successfully symlinks the files

--- DotFiles/AppDelegate.swift
+++ DotFiles/AppDelegate.swift
@@ -23,21 +23,27 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     // Initialize all the global stuff
     var sourceFolder: NSURL = NSURL()
-    var homeFolder = NSHomeDirectory()
+    var destFolder = NSHomeDirectory()
     var manager: NSFileManager = NSFileManager()
+    var ignoredFiles = [
+      ".git": true,
+      ".gitignore": true,
+      ".DS_Store": true
+    ]
   
     func applicationDidFinishLaunching(aNotification: NSNotification?) {
       
         // Set up everything for displaying/hiding
         sourcePath.hidden = true
-        destPath.stringValue = homeFolder
+        destPath.stringValue = destFolder
         linkButton.enabled = false
     }
 
     func applicationWillTerminate(aNotification: NSNotification?) {
         // Insert code here to tear down your application
     }
-    
+  
+    // Opens a modal for selecting a source folder
     @IBAction func selectFolder(sender: AnyObject) {
       var selector: NSOpenPanel = NSOpenPanel()
       
@@ -52,19 +58,34 @@ class AppDelegate: NSObject, NSApplicationDelegate {
       }
     }
   
+    // Opens a modal for selecting a destination folder
     @IBAction func selectDest(sender: AnyObject) {
       var selector: NSOpenPanel = NSOpenPanel()
       
+      selector.canChooseDirectories = true
+      if(selector.runModal() == NSOKButton) {
+        destFolder = selector.URLs[0].path
+        destPath.stringValue = destFolder
+      }
     }
   
+  // Symlink the files
   @IBAction func symlinkFolder(sender: AnyObject) {
+    var error : NSError?
     var files = []
     files = manager.contentsOfDirectoryAtURL(sourceFolder, includingPropertiesForKeys: nil, options:nil, error:nil)
     for file in files {
       var filePath: String = file.path
+      var destination = destFolder + "/" + filePath.lastPathComponent
       println(filePath)
-      println(manager.createSymbolicLinkAtPath(filePath, withDestinationPath: homeFolder as String, error: nil))
+      println(destination)
+      let linked = manager.createSymbolicLinkAtPath(destination, withDestinationPath: filePath, error: &error)
+      if !linked{
+        if let err = error{
+          println(err.localizedDescription)
+        }
+      }
     }
   }
-}
 
+}
