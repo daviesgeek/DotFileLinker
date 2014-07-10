116250ec7e312a7287ac89beccb509afee303091
Matthew Davies <daviesgeek@icloud.com>
2014-07-10 16:57:21 -0700

    Added SnapshotManager

---
+++ DotFilesLinker/SnapshotManager.swift
@@ -0,0 +1,16 @@
+//
+//  Snapshot.swift
+//  DotFilesLinker
+//
+//  Created by Matthew Davies on 7/10/14.
+//  Copyright (c) 2014 Matthew Davies. All rights reserved.
+//
+
+import Foundation
+
+class SnapshotManager {
+  
+  func test() -> String {
+    return "hello, world"
+  }
+}
